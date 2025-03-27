de767cd21fd14220f7470cca124032c4c10d5dab
Fukua0 <1364583811@qq.com>
2025-03-27 15:04:52 +0800

    Initial commit

---
+++ MDWebManager/src/main/java/com/markdowonweb/MdWebManagerApplication.java
@@ -0,0 +1,13 @@
+package com.markdowonweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdWebManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdWebManagerApplication.class, args);
+    }
+
+}
